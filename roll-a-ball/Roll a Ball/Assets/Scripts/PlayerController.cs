8b39aab5e10533ad6dd989d404aac3ae052a37bb
dsshimel <dsshimel@gmail.com>
2017-06-25 18:08:23 -0400

    first check in with roll a ball

---
+++ roll-a-ball/Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    // called on first frame that the script is active
+    // (usually the first frame of the game)
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // do physics here
+    void FixedUpdate()
+    {
+        // float moveHorizontal = Input.GetAxis("Horizontal");
+        // float moveVertical = Input.GetAxis("Vertical");
+
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        var x = moveHorizontal;
+        var y = 0;
+        var z = moveVertical;
+        var movement = new Vector3(x, y, z);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ roll-a-ball/Roll a Ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    // Not using forces (physics), so we can spin the
+    // cube here instead of FixedUpdate()
+	void Update () {
+        // deltaTime is the time in seconds it took to complete the last frame
+        // multiplying by it makes the animation smooth and frame rate independent
+        // I guess there's an implied "per second" on the V3?
+        this.transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
