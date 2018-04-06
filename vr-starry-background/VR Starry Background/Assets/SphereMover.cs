b3f4604ac7a169bcecb21e6c2eb04f6414d49a12
dsshimel <dsshimel@gmail.com>
2018-04-06 01:16:43 -0400

    Get app working on Cardboard

--- vr-starry-background/VR Starry Background/Assets/SphereMover.cs
+++ vr-starry-background/VR Starry Background/Assets/SphereMover.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SphereMover : MonoBehaviour {
     // Min distance meteor can be from player.
--- vr-starry-background/VR Starry Background/Assets/SpinSession/Interval.cs
+++ vr-starry-background/VR Starry Background/Assets/SpinSession/Interval.cs
@@ -1,7 +1,4 @@
-﻿using UnityEngine;
-using UnityEditor;
-
-namespace SpinSession
+﻿namespace SpinSession
 {
     public class Interval
     {
