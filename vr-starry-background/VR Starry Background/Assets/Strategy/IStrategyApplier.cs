e6112f28b36a019cbf8b08eee1da13dd00e1da6a
dsshimel <dsshimel@gmail.com>
2018-06-03 21:27:10 -0400

    IStrategyApplier

--- vr-starry-background/VR Starry Background/Assets/Strategy/IStrategyApplier.cs
+++ vr-starry-background/VR Starry Background/Assets/Strategy/IStrategyApplier.cs
@@ -1,3 +1,5 @@
 ﻿public interface IStrategyApplier<T>
 {
+    void Apply(IStrategy<T> strategy, float timeNow, float timeBefore);
+    void ApplyFade(IStrategy<T> strategyOut, IStrategy<T> strategyIn, float fadeOutPercent, float timeNow, float timeBefore);
 }
